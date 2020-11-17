6596e530d20d1368bf0aec73d55bc3fb9d61579a
Ayaka Hidaka <ayakahidaka@AyakanoMacBook-Pro.local>
2020-11-17 22:51:26 +0900

    Initialized repository

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import Example from '../components/Example.vue';
+
+const app = new Vue({
+  el: "#app",
+  components: {
+    Example,
+  },
+	data: {
+		message: 'Hello World'
+	}
+})
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ],
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  }
+}
