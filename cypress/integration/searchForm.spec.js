4561424aa687ec47b7ab73df8a00b50ab60382ce
Ayaka Hidaka <ayakahidaka@AyakanoMacBook-Pro.local>
2021-04-05 21:57:21 +0900

    searchForm.spec.jsにテストケース追加

--- cypress/integration/searchForm.spec.js
+++ cypress/integration/searchForm.spec.js
@@ -7,4 +7,13 @@ describe('Search form', () => {
     cy.contains('Model Plans').click();
     cy.location('pathname').should('eq', '/model_plans');
   });
+
+  it('目的地を入力して検索できること', () => {
+    cy.visit(baseUrl);
+
+    cy.get('input[name="destination"]').type('博多駅');
+    cy.contains('Add Destination').click();
+    cy.get('input[name="destination"]').type('小倉駅');
+    // cy.location('pathname').should('eq', '/model_plans');
+  });
 })
